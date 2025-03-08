321b37d84fe93b2c65b0c9235399cff865da3a58
samuelbslawrence <samuel.b.l.skelton@gmail.com>
2025-03-08 21:57:46 +0000

    First Lab Done

--- Lab Work/Lab 1/Client/Client/Program.cs
+++ Lab Work/Lab 1/Client/Client/Program.cs
@@ -1,31 +1,46 @@
-﻿using System;
+﻿// CLIENT
+using System;
 using System.Net.Sockets;
 using System.Text;
 
-TcpClient tcpClient = new TcpClient();
-tcpClient.Connect("127.0.0.1", 5000);
-NetworkStream nStream = tcpClient.GetStream();
-
-Console.WriteLine("Enter a message to be translated...");
-string? message = Console.ReadLine();
-
-if (message != null)
+class Client
 {
-    byte[] request = Serialize(message);
-    nStream.Write(request, 0, request.Length);
-    // TODO: Read response from stream and display to user
-}
+    static void Main()
+    {
+        TcpClient tcpClient = new TcpClient();
+        tcpClient.Connect("127.0.0.1", 5000);
+        NetworkStream nStream = tcpClient.GetStream();
 
-Console.ReadKey(); // Wait for keypress before exit
+        Console.WriteLine("Enter a message to be translated:");
+        string message = Console.ReadLine() ?? string.Empty;
 
-byte[] Serialize(string request)
-{
-    byte[] responseBytes = Encoding.ASCII.GetBytes(request);
-    byte responseLength = (byte)responseBytes.Length;
-    
-    byte[] rawData = new byte[responseLength + 1];
-    rawData[0] = responseLength;
-    responseBytes.CopyTo(rawData, 1);
+        if (!string.IsNullOrEmpty(message))
+        {
+            byte[] request = Serialize(message);
+            nStream.Write(request, 0, request.Length);
 
-    return rawData;
+            string response = ReadFromStream(nStream);
+            Console.WriteLine("Translated message: " + response);
+        }
+
+        tcpClient.Close();
+    }
+
+    static byte[] Serialize(string request)
+    {
+        byte[] requestBytes = Encoding.ASCII.GetBytes(request);
+        byte requestLength = (byte)requestBytes.Length;
+        byte[] rawData = new byte[requestLength + 1];
+        rawData[0] = requestLength;
+        requestBytes.CopyTo(rawData, 1);
+        return rawData;
+    }
+
+    static string ReadFromStream(NetworkStream stream)
+    {
+        int responseLength = stream.ReadByte();
+        byte[] responseBytes = new byte[responseLength];
+        stream.Read(responseBytes, 0, responseLength);
+        return Encoding.ASCII.GetString(responseBytes);
+    }
 }
--- Lab Work/Lab 1/Server/Server/Program.cs
+++ Lab Work/Lab 1/Server/Server/Program.cs
@@ -1,37 +1,79 @@
-﻿using System;
+﻿// SERVER
+using System;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
-TcpListener tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 5000);
-tcpListener.Start();
-TcpClient tcpClient = tcpListener.AcceptTcpClient(); NetworkStream nStream =
-tcpClient.GetStream();
-
-string message = ReadFromStream(nStream);
-Console.WriteLine("Received: \"" + message + "\"");
-
-string translatedMessage = Translate(message);
-
-// TODO: Serialize the translated message and write it to the stream
-
-Console.ReadKey(); // Wait for keypress before exit
-
-static string Translate(string message)
+class Server
 {
-    string translatedmessage = "TEST RESPONSE";
-    string[] words = message.Split(' ');
-    foreach (string word in words)
+    static void Main()
     {
-        // TODO: Perform translation
-    }
-    return translatedmessage;
-}
+        TcpListener tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), 5000);
+        tcpListener.Start();
+        Console.WriteLine("Server started. Waiting for connection...");
 
-static string ReadFromStream(NetworkStream stream)
-{
-    int messageLength = stream.ReadByte();
-    byte[] messageBytes = new byte[messageLength];
-    stream.Read(messageBytes, 0, messageLength);
-    return Encoding.ASCII.GetString(messageBytes);
+        TcpClient tcpClient = tcpListener.AcceptTcpClient();
+        NetworkStream nStream = tcpClient.GetStream();
+
+        string message = ReadFromStream(nStream);
+        Console.WriteLine("Received: \"" + message + "\"");
+
+        string translatedMessage = Translate(message);
+        Console.WriteLine("Translated: \"" + translatedMessage + "\"");
+
+        byte[] response = Serialize(translatedMessage);
+        nStream.Write(response, 0, response.Length);
+
+        tcpClient.Close();
+        tcpListener.Stop();
+    }
+
+    static string Translate(string message)
+    {
+        string[] words = message.Split(' ');
+        StringBuilder translatedMessage = new StringBuilder();
+
+        foreach (string word in words)
+        {
+            string translatedWord = ConvertToPigLatin(word);
+            translatedMessage.Append(translatedWord + " ");
+        }
+
+        return translatedMessage.ToString().Trim();
+    }
+
+    static string ConvertToPigLatin(string word)
+    {
+        if (string.IsNullOrEmpty(word)) return word;
+
+        string vowels = "AEIOUaeiou";
+        int index = 0;
+
+        while (index < word.Length && !vowels.Contains(word[index]))
+            index++;
+
+        if (index == 0)
+            return word + "way";
+        else
+            return word.Substring(index) + word.Substring(0, index) + "ay";
+    }
+
+    static string ReadFromStream(NetworkStream stream)
+    {
+        int messageLength = stream.ReadByte();
+        byte[] messageBytes = new byte[messageLength];
+        stream.Read(messageBytes, 0, messageLength);
+        return Encoding.ASCII.GetString(messageBytes);
+    }
+
+    static byte[] Serialize(string response)
+    {
+        byte[] responseBytes = Encoding.ASCII.GetBytes(response);
+        byte responseLength = (byte)responseBytes.Length;
+        byte[] rawData = new byte[responseLength + 1];
+        rawData[0] = responseLength;
+        responseBytes.CopyTo(rawData, 1);
+        return rawData;
+    }
 }
