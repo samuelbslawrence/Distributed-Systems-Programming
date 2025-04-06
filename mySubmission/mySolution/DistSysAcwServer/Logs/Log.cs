da437a4a943bb19c386f8df5325b7faabf76237c
samuelbslawrence <samuel.b.l.skelton@gmail.com>
2025-04-06 21:38:59 +0100

    Update 5

---
+++ mySubmission/mySolution/DistSysAcwServer/Logs/Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DistSysAcwServer.Models
+{
+    #region Task 13
+    // Log entity representing a user action log
+    public class Log
+    {
+        [Key]
+        public int LogID { get; set; } // Primary key
+
+        public string LogString { get; set; } // Description of the log
+
+        public DateTime LogDateTime { get; set; } // Timestamp of the log
+
+        // Constructor that sets the log message and current time
+        public Log(string logMessage)
+        {
+            LogString = logMessage;
+            LogDateTime = DateTime.Now;
+        }
+
+        // Parameterless constructor for EF Core
+        public Log() { }
+    }
+    #endregion
+}
---
+++ mySubmission/mySolution/DistSysAcwServer/Logs/LogArchive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DistSysAcwServer.Models
+{
+    #region Task 13
+    // LogArchive entity to store logs of deleted users
+    public class LogArchive
+    {
+        [Key]
+        public int LogID { get; set; } // Primary key
+
+        public string LogString { get; set; } // Log description
+
+        public DateTime LogDateTime { get; set; } // Timestamp of the log
+
+        public string UserApiKey { get; set; } // API key of the user to whom this log belonged
+
+        // Constructor that sets the log message, associated user API key and current time
+        public LogArchive(string logMessage, string userApiKey)
+        {
+            LogString = logMessage;
+            LogDateTime = DateTime.Now;
+            UserApiKey = userApiKey;
+        }
+
+        // Parameterless constructor for EF Core
+        public LogArchive() { }
+    }
+    #endregion
+}
